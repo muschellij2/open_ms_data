3a9594837852e1bdb8a27522f53ac4fcaedb4ce9
muschellij2 <muschellij2@gmail.com>
2018-02-26 10:54:01 -0500

    added readme

---
+++ cross_sectional/README.md
@@ -0,0 +1,43 @@
+## 3D MR image database of Multiple Sclerosis patients with white matter lesion segmentations
+
+
+
+This archive contains Magnetic Resonance (MR) images of Multiple 
+Sclerosis (MS) patients with corresponding consensus based ground
+truth segmentations of white matter lesions. The images are grouped 
+into folders for each of the patients. Each patient's folder consists of:
+
+- Co-registered and bias corrected T1-weighted (T1W), contrast enhanced T1-weighted (T1WKS), T2-weighted (T2W) and FLAIR images (co-registered to FLAIR space)
+- Brain mask (in FLAIR space)
+- Consensus white matter lesion segmentations (in FLAIR space)
+- RAW images in their original space 
+- Intra-study transform parameters (rigid to FLAIR)
+
+To preserve patients' privacy **all images have been defaced**. 
+For more details please see the *References*.
+
+*Published on: http://lit.fe.uni-lj.si/tools*
+
+
+
+### Folder structure ###
+
+- `patientXX/`
+    - `patientXX_brainmask.nii.gz` (Brain mask)
+    - `patientXX_consensus_gt.nii.gz` (Consensus white matter lesion segmentations)
+    - `patientXX_modality.nii.gz` (Spatially co-registered and bias corrected T1, T1KS, T2, FLAIR images)
+    - `patientXX_modality_to_FLAIR.txt` (Affine transform parameters to register images into FLAIR space)
+    - `raw/`
+        - `patientXX_modality.nii.gz` (RAW images in their original space)
+
+
+
+### License ###
+
+The database is released under the Creative-Commons Attribution (CC-BY) license. Please cite the references below in any published work that uses this database.
+
+
+
+### References ###
+
+- Lesjak, Žiga, Franjo Pernuš, Boštjan Likar, and  Žiga Špiclin. "A Novel Public MR Image Dataset of Multiple Sclerosis Patients With Lesion Segmentations Based on Multi-rater Consensus", Neuroinformatics (2017), DOI: 10.1007/s12021-017-9348-7
---
+++ programs/process_cs_smri.R
@@ -0,0 +1,138 @@
+#!/usr/bin/env RScript
+#######################
+# Processing Cross-sectional data
+#######################
+rm(list = ls())
+# library(dcmtk)
+# library(dcm2niir)
+# library(fslr)
+# library(divest)
+library(dplyr)
+library(matrixStats)
+library(smri.process)
+library(here)
+
+rootdir = here::here()
+proc_dir = file.path(rootdir,
+    "cross_sectional", "raw")
+
+ids = list.dirs(proc_dir, 
+	recursive = FALSE)
+df = data_frame(id = basename(ids),
+  id_dir = ids)
+ids = unique(df$id)
+
+df = df %>% mutate(
+  raw_dir = file.path(id_dir, "raw"),
+  proc_dir = file.path(id_dir, 
+    "prenorm"),
+  reg_dir = file.path(id_dir, 
+    "registered"),
+  brain_malf_dir = file.path(id_dir, 
+    "brain_malf"),
+  malf_dir = file.path(id_dir, 
+    "malf")  
+)
+mods = c("FLAIR" = "FLAIR",
+  "T1" = "T1W",
+  "T2" = "T2W",
+  "T1POST" = "T1WKS",
+  "GOLD_STANDARD" = 
+  "consensus_gt")
+mod_df = sapply(mods, function(mod) {
+  fname = file.path(df$id_dir, 
+    paste0(mod, ".nii.gz"))
+})
+fe = array(
+  file.exists(mod_df),
+  dim = dim(mod_df))
+mod_df[ !fe ] = NA
+mod_df = as_data_frame(mod_df)
+mod_df = mod_df %>% 
+  mutate(have_data = rowAlls(fe))
+df = cbind(df, mod_df)
+df = as_data_frame(df)
+df = df %>% 
+  filter(have_data) %>% 
+  select(-have_data)
+
+isub = as.numeric(
+  Sys.getenv("SGE_TASK_ID")
+  )
+# idir <- as.numeric(
+#     Sys.getenv("LSB_JOBINDEX"))
+if (is.na(isub) || isub < 1) {
+  isub = 1
+}
+
+idf = df[isub,]
+dir.create(idf$proc_dir,
+  showWarnings = FALSE)
+dir.create(idf$reg_dir,
+  showWarnings = FALSE)
+dir.create(idf$brain_malf_dir,
+  showWarnings = FALSE)
+dir.create(idf$malf_dir,
+  showWarnings = FALSE)
+fmods = setdiff(names(mods),
+  "GOLD_STANDARD")
+files = as.character(idf[, fmods])
+names(files) = fmods
+
+gold_standard = idf$GOLD_STANDARD
+
+print(isub)
+print(idf$id)
+
+outprefix = file.path(
+  idf$brain_malf_dir,
+  "FLAIR_")
+
+processed = smri_prenormalize(
+  x = files,
+  outdir = idf$proc_dir,
+  gold_standard = gold_standard,
+  gs_space = "FLAIR",
+  reg_space = "FLAIR",
+  malf_transform = "SyN",
+  verbose = 2,
+  outprefix = outprefix,  
+  probs = c(0, 0.995),
+  num_templates = 35)
+
+
+all_resampled = seg_normalize(
+  prenormalize = processed,
+  template = "none",
+  verbose = TRUE
+)
+normalized = all_resampled$normalized
+
+pred = norm_predictors(
+  normalized = normalized,
+  normalization = "trimmed_z"
+)
+
+predictors = gather_predictors(pred)
+
+
+
+############################
+# Running the same thing
+# but with Eve
+############################
+all_resampled = seg_normalize(
+  prenormalize = processed,
+  norm_outdir = idf$reg_dir,
+  template = "Eve",
+  verbose = TRUE
+)
+normalized = all_resampled$normalized
+
+pred = norm_predictors(
+  normalized = normalized,
+  normalization = "trimmed_z"
+)
+
+predictors = gather_predictors(pred)
+
